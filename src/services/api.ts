cbc147de2b74b2e7f04c8d9a6fa16ce109bd16f0
manohar18h <manoharhambire18@gmail.com>
2025-08-23 01:17:52 -0400

    ap0i

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 const api = axios.create({
-  baseURL: API_URL,
-  headers: { "Content-Type": "application/json" },
+  baseURL: import.meta.env.VITE_API_URL, // ✅ from .env
 });
 
 export default api;
